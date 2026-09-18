d871bc6cfcb549322f75563d007a72449b06ad08
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-09-18 01:56:33 +0100

    set up server connection

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello via Bun!");
+import app from "./src/app";
+import mongoose from "mongoose";
+import { connectDB } from "./src/config/database";
+import http from "http";
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+const startServer = async () => {
+  try {
+    // connect to mongoDB
+    connectDB();
+    mongoose.connection.once("open", () => {
+      console.log("Connected to MongoDB ☑️");
+      server.listen(PORT, () => {
+        console.log(`Server running on port ${PORT} 🚀🚀`);
+      });
+    });
+  } catch (error) {
+    console.error(
+      `Error: ${error instanceof Error ? error.message : String(error)}`,
+    );
+  }
+};
+
+startServer();
