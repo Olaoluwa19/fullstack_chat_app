d3a2fc3cf3a654b432d94ff0383f29251c0c2711
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-09-21 00:21:54 +0100

    refactor: update Chat and User models for better type handling

--- backend/src/models/Chat.ts
+++ backend/src/models/Chat.ts
@@ -2,7 +2,7 @@ import mongoose, { Schema, type Document } from "mongoose";
 
 export interface IChat extends Document {
   participants: mongoose.Types.ObjectId[];
-  lastMessage?: mongoose.Types.ObjectId;
+  lastMessage?: mongoose.Types.ObjectId | null;
   lastMessageAt?: Date;
   createdAt: Date;
   updatedAt: Date;
@@ -17,13 +17,11 @@ const ChatSchema = new Schema<IChat>(
         required: true,
       },
     ],
-    lastMessage: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Message",
-        default: null,
-      },
-    ],
+    lastMessage: {
+      type: Schema.Types.ObjectId,
+      ref: "Message",
+      default: null,
+    },
     lastMessageAt: {
       type: Date,
       default: Date.now(),
