3cbb99c6f4d8801650231be0ea3c3aeb6f2cda80
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-09-23 17:29:11 +0100

    feat: implement chat and message retrieval, and user listing routes

--- backend/src/routes/chatRoute.ts
+++ backend/src/routes/chatRoute.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
+import { protectRoute } from "../middleware/auth";
+import { getChats, getOrCreateChat } from "../controllers/chatController";
 
 const router = Router();
+router.use(protectRoute);
+
+router.route("/").get(getChats);
+router.route("/with/:participantId").get(getOrCreateChat);
 
 export default router;
