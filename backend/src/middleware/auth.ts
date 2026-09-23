974fbdca636da58307cf2f72edf94da0217b8275
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-09-23 10:38:16 +0100

    updated how error is handled in auth.ts

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -28,7 +28,6 @@ export const protectRoute = async (
 
     next();
   } catch (error) {
-    console.error("Error in protectRoute middleware", error);
-    res.status(500).json({ message: "Internal server error" });
+    next(error);
   }
 };
